206d0809f345c7c65f13bc82889bb6b7d469570c
Oliver Song <me@oliversong.net>
2022-01-01 14:23:57 -0800

    classify

--- write_to_acep.py
+++ write_to_acep.py
@@ -16,53 +16,56 @@ import traceback
 logging.basicConfig(level=logging.DEBUG)
 
 pic = 'test.bmp'
-awake = False
 
 def bitmapitize():
     # TODO: convert a normal image into a X by Y bitmap with 7 colors
     pass
 
-def render_pic(epd_instance):
-    # TODO: pass in pic received from hook
-    try:
-        logging.info("attempting render")
-        if awake = False:
-            logging.info("module asleep, waking")
-            epd5in65f.epdconfig.module_init()
-        image = Image.open(os.path.join(os.path.dirname(os.path.realpath(__file__)), pic))
-        epd_instance.display(epd_instance.getbuffer(image))
+class AcepController:
+    def __init__(self, epd_instance):
+        self.awake = False
+        self.epd_instance = epd_instance
 
-        time.sleep(3)
+    def render_pic(self):
+        # TODO: pass in pic received from hook
+        try:
+            logging.info("attempting render")
+            if self.awake == False:
+                logging.info("module asleep, waking")
+                epd5in65f.epdconfig.module_init()
+            image = Image.open(os.path.join(os.path.dirname(os.path.realpath(__file__)), pic))
+            self.epd_instance.display(self.epd_instance.getbuffer(image))
 
-        logging.info("sleep")
-        epd_instance.sleep()
+            time.sleep(3)
 
-    except IOError as e:
-        logging.info(e)
+            logging.info("sleep")
+            self.epd_instance.sleep()
 
-    except KeyboardInterrupt:
-        logging.info("ctrl + c:")
-        epd5in65f.epdconfig.module_exit()
-        awake = False
-        exit()
+        except IOError as e:
+            logging.info(e)
 
-def init_display():
-    try:
-        epd_instance = epd5in65f.EPD()
-        logging.info("init and Clear")
-        epd_instance.init()
-        epd_instance.Clear()
-        awake = True
-        return epd_instance
+        except KeyboardInterrupt:
+            logging.info("ctrl + c:")
+            epd5in65f.epdconfig.module_exit()
+            self.awake = False
+            exit()
 
-    except IOError as e:
-        logging.info(e)
+    def init_display(self):
+        try:
+            self.epd_instance = epd5in65f.EPD()
+            logging.info("init and Clear")
+            self.epd_instance.init()
+            self.epd_instance.Clear()
+            self.awake = True
+            return self.epd_instance
 
-    except KeyboardInterrupt:
-        logging.info("ctrl + c:")
-        epd5in65f.epdconfig.module_exit()
-        exit()
+        except IOError as e:
+            logging.info(e)
 
+        except KeyboardInterrupt:
+            logging.info("ctrl + c:")
+            epd5in65f.epdconfig.module_exit()
+            exit()
 
 def main():
     try:
